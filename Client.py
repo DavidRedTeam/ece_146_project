84312c2155a54b6dda085cd6870f67972eab0978
David <andraded95@yahoo.com>
2022-04-05 23:45:05 -0700

    Update Client.py
    Created a dummyrouter.py on my local machine to establish a connection for now. Will work on ip header and frame header next.

--- Client.py
+++ Client.py
@@ -1,12 +1,26 @@
 #Client that will connect to server through routers.
 import socket
 import time
+#This clients mac and ip static.
 client_ip = "192.168.1.2"
 client_mac = "12:AB:6A:BA:DD:C6"
 
+#Identifying the router we want to connect to. *Note that the socket will act as the ip since we are
+#defining the client, routers, and server on the same computer.
 router1 = ("LocalHost", 2000)
-router2 = ("LocalHost", 2001)
 
-client = socket.socket(AF_INET, socket.SOCK_STREAM)
+#make the socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#connect to the router
 client.connect(router1) #not created yet but waiting to merge code.
+
+#This will handle the message we send and receive.
+print(("Please enter a message to send"))
+connected = True
+while connected:
+    message = input()
+    if message.find("quit") != -1: connected = False
+    client.sendall(bytes(message, "utf-8"))
+    recv_message = client.recv(1024).decode("utf-8")
+    print(recv_message)
