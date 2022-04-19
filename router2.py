bb362db7eabbac1ce2eb609b5ecfc14a377ec6b7
hcotton890 <101040361+hcotton890@users.noreply.github.com>
2022-04-18 20:12:11 -0700

    Routing table + hops

--- router2.py
+++ router2.py
@@ -1,11 +1,17 @@
-# has connection from router1 needs to connect to router3
-
 import socket
 import time
 
-router22router1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-router22router1.bind(("Localhost", 2002))
+#hop metric
+hop_count_router1 = 2
+hop_count_router3 = 1
 
+router1 = ("LocalHost", 2002)
+
+#if(hop_count_router1 < hop_count_router3):
+router22router1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+router22router1.bind(router1)
+
+#if(hop_count_router1 > hop_count_router3):
 router3 = ("LocalHost", 2003)
 router22router3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 router22router3.connect(router3)
@@ -20,17 +26,24 @@ router2_mac = "05:10:0A:DC:35:AF"
 router3_ip = "192.168.1.4"
 router3_mac = "05:10:0A:DF:5A:4A"
 
+server_ip = "192.168.0.1"
+server_mac = "12:AB:6A:DD:C10"
+
 # connect to router3
 # router3 = ("Localhost", 2003)
+def bandwidth_scaling(bandwidth):
+	bandwidth_scaled = (10000000/bandwidth) * 256
+	return bandwidth_scaled
 
+
+def delay_scaling(delay):
+	delay_scaled = delay * 256
+	return delay_scaled
 # Listen for router1 connection
 
 router22router1.listen(1)
 
 # router1 = None
-
-
-
 
 # while (router1 == None):
 router2Router, address = router22router1.accept()
@@ -40,7 +53,31 @@ router2Router, address = router22router1.accept()
 if router2Router:
 	print("Router 1 is connected")
 
+def create_route(destination, next_hop, hop_count):
+    return routes(destination, next_hop, hop_count)
 
+
+class routes:
+    def __init__(self, destination, next_hop, hop_count):
+        self.destination = destination
+        self.next_hop = next_hop
+        self.hop_count = hop_count
+
+    def getDestionation(self):
+        return self.destination
+
+    def getnext_hop(self):
+        return self.next_hop
+
+    def gethop_count(self):
+        return self.hop_count
+
+route2to1 = 1
+route2to3 = 1
+
+router_table = []
+router_table.append(create_route(server_ip, router1, route2to1))
+router_table.append(create_route(server_ip, router3, route2to3))
 # TODO: FIX ARP TABLE TO INCLUDE THE OTHER ROUTERS
 # arp_table_socket = {router1_ip: router1}
 # arp_table_mac = {router1_ip: router1_mac}
